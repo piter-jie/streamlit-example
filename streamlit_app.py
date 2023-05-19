7a58f0e3a4bc2b28c8cde4243f6c3b00ce5eba7a
piter-jie <109156186+piter-jie@users.noreply.github.com>
2023-05-19 20:48:15 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -13,55 +13,53 @@ import matplotlib.pyplot as plt
 import time
 import numpy as np
 import streamlit as st
+page = st.sidebar.selectbox("选择页面", ("在线故障诊断", "模型预测性能展示"))
 
-# Streamlit widgets automatically run the script from top to bottom. Since
-# this button is not connected to any other logic, it just causes a plain
-# rerun.
-#upload_file = st.file_uploader("选择一个测试文件传入",type=['.npy']) 
-# 读取文件为字节流
-#file_bytes = upload_file.read()
-#if upload_file is None:
-    #st.write('No file name provided')
-#else:
+# 根据选择的页面显示不同的内容
+if page == "在线故障诊断":
+    #st.subheader("页面 1")
+    # 页面 1 的内容
+    #st.write("这是页面 1 的内容")
+
+    upload_file = st.file_uploader(
+        label = "选择一个测试文件传入",type=['.npy']
+    )
+
+    if upload_file is not None:
+        # 不为空
+        data = np.load(upload_file)
+        st.success("上传文件成功！")
+    else:
+        st.stop() # 退出      
+    # 加载为numpy数组  
+    #data = np.load(io.BytesIO(file_bytes))  
     #data = np.load(upload_file)
+    # 画信号波形图
+    figure, axes = plt.subplots()
+    axes.plot(data[:3000])
+    axes.set_title("Signal at Point A")
+    axes.set_xlabel("Time")
+    axes.set_ylabel("Amplitude")
+    figure.tight_layout()
 
+    st.write("Signal Plot at Point A")
+    st.pyplot(figure)
 
-
-upload_file = st.file_uploader(
-    label = "选择一个测试文件传入",type=['.npy']
-)
-
-if upload_file is not None:
-    # 不为空
-    data = np.load(upload_file)
-    st.success("上传文件成功！")
-else:
-    st.stop() # 退出      
-# 加载为numpy数组  
-#data = np.load(io.BytesIO(file_bytes))  
-#data = np.load(upload_file)
-# 画信号波形图
-figure, axes = plt.subplots()
-axes.plot(data[:3000])
-axes.set_title("Signal at Point A")
-axes.set_xlabel("Time")
-axes.set_ylabel("Amplitude")
-figure.tight_layout()
-
-st.write("Signal Plot at Point A")
-st.pyplot(figure)
-
-#显示进度条
-#st.write("10. st.progress()")
-import time
-st.write("正在检测故障类型")
-# 添加placeholder
-latest_iteration = st.empty()
-bar = st.progress(0)
-for i in range(100):
-    # 更新进度条
-    latest_iteration.text(f'Iteration {i+1}')
-    bar.progress(i+1)
-    time.sleep(0.1)
-'运行结束!'
-st.markdown('故障类型检测结果：正常')
+    #显示进度条
+    #st.write("10. st.progress()")
+    import time
+    st.write("正在检测故障类型")
+    # 添加placeholder
+    latest_iteration = st.empty()
+    bar = st.progress(0)
+    for i in range(100):
+        # 更新进度条
+        latest_iteration.text(f'Iteration {i+1}')
+        bar.progress(i+1)
+        time.sleep(0.1)
+    '运行结束!'
+    st.markdown('故障类型检测结果：正常')
+elif page == "模型预测性能展示":
+    st.subheader("页面 2")
+    # 页面 2 的内容
+    st.write("这是页面 2 的内容")
